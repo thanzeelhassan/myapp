be9f92da4f40918ebe5a855b1184444a976be914
Thanzeel Hassan <46446324+thanzeelhassan@users.noreply.github.com>
2021-02-14 20:14:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# myapp
+ first flutter app
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main()  => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('My first App'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('Hello Ninjas!'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('Click'),
+    ),
+  ),
+));
+
+
