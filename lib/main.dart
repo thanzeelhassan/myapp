c57cea6574d6c47c77b2e0764000fad4bf3d0ccd
Thanzeel Hassan <46446324+thanzeelhassan@users.noreply.github.com>
2021-02-15 17:10:59 +0530

    video 8 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main()  => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My first App'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('Hello Ninjas!'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('Click'),
-    ),
-  ),
+  home: Home()
 ));
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: FlatButton(
+          onPressed: () {
+            print('you clicked me');
+          },
+          child: Text('Click me'),
+          color: Colors.lightBlue,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text('Click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
 
